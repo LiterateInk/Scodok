a09674184c250e4e57820a305578f88e964502b5
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-30 23:15:08 +0100

    docs(examples): update `cas`

--- examples/rust/src/bin/cas.rs
+++ examples/rust/src/bin/cas.rs
@@ -1,11 +1,15 @@
-use scodok::Session;
 use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  let session = Session::new("https://notes9.iutlan.univ-rennes1.fr");
-  let url = scodok::retrieve_cas_url(&session).await?;
-  println!("{url}");
+  let instance_url = "https://notes.scodoc.local/";
+  let cas_url = scodok::retrieve_cas_url(instance_url).await?;
+  println!("You have to authenticate to your CAS at the following URL: {cas_url}");
+
+  // Let's say we grabbed this variable from the CAS authentication...
+  let ticket = "ST-123456789-SOMELONGSTRING-vmjava-pcas2";
+  let session = scodok::process_cas_ticket(instance_url, ticket).await?;
+  println!("You are now authenticated in ScoDoc Notes with the following PHP session ID: {}", session.php_sess_id());
 
   Ok(())
 }
--- examples/typescript/src/cas.ts
+++ examples/typescript/src/cas.ts
@@ -1,5 +1,10 @@
-import { Session, retrieveCasUrl } from "scodok";
+import { retrieveCasUrl, processCasTicket } from "scodok";
 
-const session = new Session("https://notes9.iutlan.univ-rennes1.fr");
-const url = await retrieveCasUrl(session);
-console.log(url);
+const instanceUrl = "https://notes.scodoc.local/";
+const casUrl = await retrieveCasUrl(instanceUrl);
+console.log("You have to authenticate to your CAS at the following URL:", casUrl);
+
+// Let's say we grabbed this variable from the CAS authentication...
+const ticket = "ST-123456789-SOMELONGSTRING-vmjava-pcas2";
+const session = await processCasTicket(instanceUrl, ticket);
+console.log("You are now authenticated in ScoDoc Notes with the following PHP session ID:", session.phpSessId);

11b262c05eab175277942f5172d48214490234cb
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-31 05:06:02 +0100

    refactor(session): wider support and add `fetcher` in wasm32

--- src/session.rs
+++ src/session.rs
@@ -1,30 +1,76 @@
-#[derive(Debug, Clone)]
-#[cfg_attr(feature = "ffi", derive(uniffi::Object))]
-#[cfg_attr(target_arch = "wasm32", wasm::export)]
+use fetcher::HeaderMap;
+
+#[cfg(target_arch = "wasm32")]
+#[wasm::export]
 pub struct Session {
   instance_url: String,
   php_sess_id: String,
+  fetcher: js_sys::Function
 }
 
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg_attr(feature = "ffi", derive(uniffi::Object))]
+pub struct Session {
+  pub instance_url: String,
+  pub php_sess_id: String,
+}
+
+#[cfg(not(target_arch = "wasm32"))]
 #[cfg_attr(feature = "ffi", uniffi::export)]
-#[cfg_attr(target_arch = "wasm32", wasm::export)]
 impl Session {
   #[cfg_attr(feature = "ffi", uniffi::constructor)]
-  #[cfg_attr(target_arch = "wasm32", wasm_bindgen(constructor))]
   pub fn new(instance_url: &str, php_sess_id: &str) -> Self {
     Self {
       instance_url: instance_url.to_string(),
       php_sess_id: php_sess_id.to_string(),
     }
   }
+}
 
-  #[cfg_attr(target_arch = "wasm32", wasm_bindgen(getter = instanceUrl))]
-  pub fn instance_url(&self) -> String {
+#[cfg(target_arch = "wasm32")]
+#[wasm::export]
+impl Session {
+  #[cfg_attr(target_arch = "wasm32", wasm_bindgen(constructor))]
+  pub fn new(instance_url: &str, php_sess_id: &str, fetcher: js_sys::Function) -> Self {
+    Self {
+      instance_url: instance_url.to_string(),
+      php_sess_id: php_sess_id.to_string(),
+      fetcher
+    }
+  }
+
+  #[wasm_bindgen(getter = instanceUrl)]
+  pub fn _instance_url(&self) -> String {
     self.instance_url.clone()
   }
 
-  #[cfg_attr(target_arch = "wasm32", wasm_bindgen(getter = phpSessId))]
-  pub fn php_sess_id(&self) -> String {
+  #[wasm_bindgen(getter = phpSessId)]
+  pub fn _php_sess_id(&self) -> String {
     self.php_sess_id.clone()
+  }
+}
+
+impl Session {
+  pub fn instance_url(&self) -> &str {
+    &self.instance_url
+  }
+
+  pub fn php_sess_id(&self) -> &str {
+    &self.php_sess_id
+  }
+
+  #[cfg(target_arch = "wasm32")]
+  pub fn fetcher(&self) -> &js_sys::Function {
+    &self.fetcher
+  }
+
+  pub fn get_headers(&self) -> HeaderMap {
+    // We only need the PHPSESSID cookie.
+    let cookies = "PHPSESSID=".to_string() + &self.php_sess_id;
+
+    // Build the headers with that cookie !
+    let mut headers = HeaderMap::new();
+    headers.insert("Cookie", cookies.parse().unwrap());
+    headers
   }
 }

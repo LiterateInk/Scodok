37d6171e017c7f59f234c88dd5d437027aa955d6
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-31 05:06:35 +0100

    refactor(cas): use new utils API and also pass fetcher to session depending on target

--- src/cas.rs
+++ src/cas.rs
@@ -1,15 +1,10 @@
 use fetcher::{fetch, Method, Request, Url};
-use crate::Session;
+use crate::{Session, Error};
 
 /// Retrieve the CAS URL from the instance URL.
-///
-/// You'll need to implement the CAS authentication for the provided URL yourself.
-///
-/// After this, you can call the [`process_cas_ticket`] function to
-/// authenticate in Scodoc Notes.
 #[cfg_attr(feature = "ffi", uniffi::export)]
 #[cfg_attr(target_arch = "wasm32", wasm::append_fetcher, wasm::export)]
-pub async fn retrieve_cas_url (instance_url: &str) -> Result<String, crate::Error> {
+pub async fn get_cas_url (instance_url: &str) -> Result<String, Error> {
   let mut url = Url::parse(instance_url).unwrap();
   url.set_path("/services/doAuth.php");
 
@@ -17,20 +12,21 @@ pub async fn retrieve_cas_url (instance_url: &str) -> Result<String, crate::Erro
     url,
     method: Method::GET,
     headers: Default::default(),
-    follow: Some(false),
+    follow: false,
     body: None,
   };
 
   let response = fetch!(request);
   let headers = response.headers();
-  let location = headers.get("location").ok_or(crate::Error::InvalidRedirection())?;
+  let location = headers.get("location").ok_or(Error::InvalidRedirection())?;
 
   Ok(location.to_str().unwrap().into())
 }
 
+/// Process the CAS ticket to get the session.
 #[cfg_attr(feature = "ffi", uniffi::export)]
 #[cfg_attr(target_arch = "wasm32", wasm::append_fetcher, wasm::export)]
-pub async fn process_cas_ticket (instance_url: &str, ticket: &str) -> Result<Session, crate::Error> {
+pub async fn get_session_from_cas_ticket (instance_url: &str, ticket: &str) -> Result<Session, Error> {
   let mut url = Url::parse(instance_url).unwrap();
   url.set_path("/services/doAuth.php");
   url.query_pairs_mut().clear()
@@ -41,20 +37,26 @@ pub async fn process_cas_ticket (instance_url: &str, ticket: &str) -> Result<Ses
     url,
     method: Method::GET,
     headers: Default::default(),
-    follow: Some(false),
+    follow: false,
     body: None,
   };
 
   let response = fetch!(request);
 
   if response.status != 302 {
-    return Err(crate::Error::InvalidCasTicket());
+    return Err(Error::InvalidCasTicket());
   }
 
   let headers = response.headers();
-  let set_cookie = headers.get("set-cookie").ok_or(crate::Error::InvalidCasTicket())?;
+  let set_cookie = headers.get("set-cookie").ok_or(Error::InvalidCasTicket())?;
   let cookie = set_cookie.to_str().unwrap().split(';').next().unwrap();
-  let php_sessid = cookie.split('=').last().unwrap();
+  let php_sess_id = cookie.split('=').last().unwrap();
 
-  Ok(Session::new(instance_url, php_sessid))
+  #[cfg(target_arch = "wasm32")]
+  let session = Session::new(instance_url, php_sess_id, fetcher);
+
+  #[cfg(not(target_arch = "wasm32"))]
+  let session = Session::new(instance_url, php_sess_id);
+
+  Ok(session)
 }

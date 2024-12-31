913be814f392b140c0001ae16bc21997b4ca9315
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-31 05:07:19 +0100

    chore: exports and rename

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,13 @@ mod error;
 pub use error::Error;
 
 mod cas;
-pub use cas::{retrieve_cas_url, process_cas_ticket};
+pub use cas::{get_cas_url, get_session_from_cas_ticket};
+
+mod token;
+pub use token::get_session_from_token;
+
+mod data;
+pub use data::get_profile_picture_bytes;
 
 mod session;
 pub use session::Session;

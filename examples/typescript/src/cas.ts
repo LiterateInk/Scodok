00f8324a28dd9722e4abc39e445fff8e819c33c4
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-12-31 05:08:10 +0100

    docs(examples/ts): rewrite function name

--- examples/typescript/src/cas.ts
+++ examples/typescript/src/cas.ts
@@ -1,10 +1,10 @@
-import { retrieveCasUrl, processCasTicket } from "scodok";
+import { getCasUrl, getSessionFromCasTicket } from "scodok";
 
 const instanceUrl = "https://notes.scodoc.local/";
-const casUrl = await retrieveCasUrl(instanceUrl);
+const casUrl = await getCasUrl(instanceUrl);
 console.log("You have to authenticate to your CAS at the following URL:", casUrl);
 
 // Let's say we grabbed this variable from the CAS authentication...
 const ticket = "ST-123456789-SOMELONGSTRING-vmjava-pcas2";
-const session = await processCasTicket(instanceUrl, ticket);
+const session = await getSessionFromCasTicket(instanceUrl, ticket);
 console.log("You are now authenticated in ScoDoc Notes with the following PHP session ID:", session.phpSessId);

3b9fddf870e40794f3fdeca05448b52f1e0d5e5a
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-02 17:40:53 +0100

    chore(def): exports

--- src/definitions/mod.rs
+++ src/definitions/mod.rs
@@ -1,5 +1,23 @@
+//! `definitions` are the API responses as-is for types.
+//!
+//! They are not meant to be used directly, but rather
+//! to be transformed into more usable data and
+//! that's the job of the `parsers` module.
+//!
+//! Some types can still be used directly and exposed publicly
+//! when the structure is simple enough and easy to read.
+
 mod response;
 pub use response::{RedirectResponse, ErrorResponse};
 
 mod user_status;
 pub use user_status::{UserStatus, UserStatusResponse};
+
+mod semester;
+pub use semester::Semester;
+
+mod config;
+pub use config::Config;
+
+mod first_authentication_data;
+pub use first_authentication_data::FirstAuthenticationDataResponse;

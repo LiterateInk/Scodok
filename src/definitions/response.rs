db6f6850c54ff81e8b5b63c8a0d0fd84d3f0c5df
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2025-01-02 17:40:13 +0100

    docs(def): response

--- src/definitions/response.rs
+++ src/definitions/response.rs
@@ -3,19 +3,24 @@
 use serde::Deserialize;
 
 /// When this is returned, we need to throw an error
-/// saying the session expired !
+/// saying the session expired.
 ///
 /// This is because a redirection means we are
 /// required to login again because of a
 /// session expiration.
 #[derive(Deserialize)]
+// We'll never read the properties
+// of this because we know it's an expired session.
+#[allow(dead_code)]
 pub struct RedirectResponse {
-  #[allow(dead_code)]
   pub redirect: String,
 }
 
+/// When this is returned, we need to throw a
+/// server error with the message provided.
+///
+/// It's kinda similar to a "Bad Request" HTTP error.
 #[derive(Deserialize)]
 pub struct ErrorResponse {
   pub erreur: String,
 }
-
